96506ac86a6d6f9a3ce79ec221d5ecfe1a7c5b8c
bjsfsmx-0544 <94998868+bjsfsmx-0544@users.noreply.github.com>
2022-06-27 14:19:02 -0500

    Traducción 1.00

--- static/js/index.js
+++ static/js/index.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 
-    console.log('Document is Ready')
+    console.log('El documento está listo')
 
-    //  getting the date using Date() object and converting it to a string
+    //  Obteniendo la fecha usando el objeto Date() y convirtiéndolos a cadena de caracteres
     let date = new Date()
     let current_date = date.toDateString()
 
-    //  display the date on the HTML page using JQUERY and JS
-    $('').text('Date : ' + current_date)
+    //  Mostrar la fecha en la página HTML usando JQUERY y JS
+    $('').text('Fecha: ' + current_date)
 
     
     let review = ""
@@ -16,34 +16,34 @@ $(document).ready(function(){
     let emotion = ""
     let emoji_url = ""
 
-    //  making a function for AJAX request
+    //  Haciendo una función para una solicitud AJAX
     function ajax_request(api_url , input_data){
 
         $.ajax({
 
-            // type of request
+            // Tipo de solicitud
             type : '',
 
-            // url
+            // URL
             url : api_url,
 
-            //  JSON data
+            // Datos JSON
             data : JSON.stringify(input_data),
 
-            //  datatype of expected response
+            // Tipo de dato de la respuesta esperada
             dataType : 'json',
 
-            //  content type
+            // Tipo de contenido
             contentType : 'application/json',
 
-            //  success method
+            //  Método "success"
             success : function(result)
             {
-                //  extract the sentiment and emoji path
+                //  Extraer la ruta de los sentimientos y emoticonos
                 emotion = result.sentiment
                 emoji_url = result.path
 
-                //  update the emoticon and sentiment accordingly
+                //  Actualizar el emoticón y los sentiminetos según corresponda
                 if (product  ==  'Smartphone'){
                     $('#m_emoji').attr('src' , emoji_url)
                     $('#m_emotion').text(emotion)
@@ -73,7 +73,7 @@ $(document).ready(function(){
                 }
             },
 
-            //  error method
+            //  Método "error"
             error : function(result)
             {
                 console.log(result)
@@ -81,12 +81,12 @@ $(document).ready(function(){
 
         })
 
-        console.log('ajax request sent')
+        console.log('Solicitud AJAX enviada')
         
     }
 
 
-    //  check if Submit button under 'smartphone' is clicked and get the review accordingly
+    //  Verificar si se hace clic en el botón para enviar debajo de "smartphone" y obtener la reseña correspondiente
     $('#m_button').click(function(){
 
         review = $('#m_textbox').val()
@@ -96,7 +96,7 @@ $(document).ready(function(){
         product = 'Smartphone'
     })
 
-    //  check if Submit button under 'camera' is clicked and get the review accordingly
+    //  Verificar si se hace clic en el botón para enviar debajo de "cámara" y obtener la reseña correspondiente
     $('').click(function(){
 
         review = $('').val()
@@ -106,7 +106,7 @@ $(document).ready(function(){
         product = 'Digital Camera'
     })
 
-    //  check if Submit button under 'headphones' is clicked and get the review accordingly
+    //  Verificar si se hace clic en el botón para enviar debajo de "audífono" y obtener la reseña correspondiente
     $('').click(function(){
 
         review = $('').val()
@@ -116,7 +116,7 @@ $(document).ready(function(){
         product = 'Headphones'
     })
 
-    //  check if Submit button under 'videogame' is clicked and get the review accordingly
+    //  Verificar si se hace clic en el botón para enviar debajo de "videojuego" y obtener la reseña correspondiente
     $('').click(function(){
 
         review = $('').val()
@@ -127,16 +127,16 @@ $(document).ready(function(){
     })
 
 
-    //  if SAVE button is clicked, hit a post request on the API
+    //  Si se hace clic en el botón "Guardar", realizar una solicitud POST a la API
 
     $('').click(function(){
 
-        console.log('save button is clicked')
+        console.log('Se ha hecho clic en el botón de guardado')
 
-        //  input data 
+        //  Datos de entrada
         input_data = {'date' : date , 'product' : product , 'review' : review , 'sentiment' : emotion}
 
-        //  ajax call
+        //  Llamada AJAX
         $.ajax({
             type : '',
             url : '',
@@ -151,7 +151,7 @@ $(document).ready(function(){
             }
         })
 
-        // clearing textboxes
+        // Limpiando las cajas de texto
         $('#m_textbox').val('')
         $('#c_textbox').val('')
         $('#h_textbox').val('')
